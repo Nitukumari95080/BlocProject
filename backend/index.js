ff26a16b3f60866d7295f187c1b2f823b5af591c
NituKumari95080 <nitukumari251001@gmail.com>
2024-12-01 14:45:38 +0530

    Connected to data base connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-
+dotenv.config();
 const app=express();
+
+
+//coonectiion mongoose
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log(`Database is connceted sucessfully`)
+}).catch((err)=>{
+    console.log(err);
+});
+
 
 app.listen(8000,()=>{
     console.log(`Server is running on port 8000!`);
