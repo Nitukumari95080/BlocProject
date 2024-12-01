cc15f66713addcb97fc93a5d15af11d87545143f
NituKumari95080 <nitukumari251001@gmail.com>
2024-12-01 10:25:31 +0530

    create all routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import SignUpForm from "./auth/forms/SignUpForm";
+import SigninForm from "./auth/forms/SigninForm";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dasboard from "./pages/Dasboard";
+import NewsArticles from "./pages/NewsArticles";
 
 const App = () => {
   return (
     <>
-      <div className="text-5xl font-bold underline text-teal-600">
-       hello
-      </div>
+      <BrowserRouter>
+      <Route>
+        <Route path="/sign-in" element={<SignUpForm/>} />
+        <Route path="/sign-up" element={<SigninForm/>} />
+        <Route path="/sign-in" element={<SignUpForm/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/dasboard" element={<Dasboard/>} />
+        <Route path="/news" element={<NewsArticles/>} />
+
+        <Route path="/" element={<Home/>} />
+      </Route>
+      </BrowserRouter>
     </>
   );
 };
